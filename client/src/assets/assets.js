d1eb99506c3e0a50382ea0123d3bf3db6e35c00e
Saif Khan <saifullahimtiaz692@gmail.com>
2026-06-07 22:49:14 +0500

    Update assets

---
+++ client/src/assets/assets.js
@@ -0,0 +1,56 @@
+export const plans = [
+    {
+        id: 'Basic',
+        price: 10,
+        credits: 100,
+        desc: 'Best for personal use.'
+    },
+    {
+        id: 'Advanced',
+        price: 50,
+        credits: 500,
+        desc: 'Best for business use.'
+    },
+    {
+        id: 'Business',
+        price: 250,
+        credits: 5000,
+        desc: 'Best for enterprise use.'
+    },
+]
+
+export const testimonialsData = [
+    {
+        name: 'Donald Jackman',
+        role: 'Graphic Designer',
+        stars: 5,
+        text: "I've been using Imagify for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.",
+    },
+    {
+        name: 'Richard Nelson',
+        role: 'Image Editor',
+        stars: 4,
+        text: "As a blogger, I require a high-quality visuals, and Imagify's AI meets my standards. Since i started using it my content has improved.",
+    },
+    {
+        name: 'Samuel Green',
+        role: 'Photographer',
+        stars: 5,
+        text: "For photography I've found Imagify to be a great tool. It helps me to create stunning images for my clients.",
+    },
+]
+
+export const stepsData = [
+    {
+        title: 'Describe Your Vision',
+        description: 'Type a phrase, sentence, or paragraph that describes the image you want to create.',
+    },
+    {
+        title: 'Watch the Magic',
+        description: 'Our AI-powered engine will transform your text into a high-quality, unique image in seconds.',
+    },
+    {
+        title: 'Download & Share',
+        description: 'Instantly download your creation or share it with the world directly from our platform.',
+    },
+]
